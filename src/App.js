bd2fb9a4eb062513a9825614e1c0b1371c4a8b0e
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-11 21:38:42 +0530

    ADDED: css for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import PayrollForm from './components/payroll-form/payroll-form';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classsName="App">
+      <Router>
+        <Switch>
+          <Route exact path="">
+            <PayrollForm />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
