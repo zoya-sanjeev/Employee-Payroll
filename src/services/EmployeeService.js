ff15f6a2e06873078b64f14c2085cb311c5f894b
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-13 01:27:12 +0530

    ADDED: ability to save employee details

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,9 +1,10 @@
 import config from '../config/config';
-import AxiosService from './axios-service';
+const axios = require('axios').default;
 
-export default class EmployeeService{
+export default class EmployeeService {
     baseUrl = config.baseUrl;
-    addEmployee(data){
-        return AxiosService.post(`${this.baseUrl}employee`, data);
+    addEmployee(data) 
+    {
+        return axios.post(`${this.baseUrl}employee`, data);
     }
 }
