c3d3c59be52a7941158810034cc415533a6bef23
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-13 16:06:42 +0530

    ADDED: service to get employees

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -3,5 +3,8 @@ class AxiosService{
     postService(url = '', payload = null,tokenRequired = false, httpOptions = null ){
         return axios.post(url, payload, tokenRequired && httpOptions);
     }
+    getService(url = '', payload = null, tokenRequired = false, httpOptions = null) {
+        return axios.get(url, payload, tokenRequired && httpOptions);
+    }
 }
 module.exports = new AxiosService()
