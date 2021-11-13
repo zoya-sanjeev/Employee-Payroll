4c7e28f329c8fbf07c53d11282707e66d6b96166
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-13 16:03:07 +0530

    ADDED: home page to app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<PayrollForm />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
